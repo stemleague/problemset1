49ff242d810dc29c241bbed3e36a033a7b545c84
stem league <64894084+stemleague@users.noreply.github.com>
2022-09-07 22:55:26 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,74 @@
+# Crack the Code: Problem Set 1
+
+# Problem: Write a function that reverses a string. The input string is given as an array of characters s.
+
+# STEP 1: Clarify your questions and assumptions
+# At a first glance at the problem above, what are some questions you have? Remember there are no dumb questions,
+# by asking questions it shows your interviewer that you are thinking about the problem and different edge cases
+# rather than jumping straight to writing code.
+#
+# TODO: Write 3 questions you have about the given problem below in the commented section:
+#
+# Question 1:
+# Question 2:
+# Question 3:
+
+
+
+# STEP 2: Whiteboarding
+# Now that you have clarifed your questions, let's go ahead and start brainstorming the different cases we can get
+# Our goal is to code up a Python function that reverses a string. What are some test cases we will expect? Let's 
+# assume we have wrote reverse_string function. What are some test cases we would expect?
+# 
+# TODO: Write 5 test cases you expect reverse_string() should output (Ex: reverse_string("hi") --> "ih"). Get 
+# creative - what if we get empty string? What about casing of the string?
+#
+# Test Case 1:
+# Test Case 2:
+# Test Case 3:
+# Test Case 4:
+# Test Case 5:
+#
+#
+#
+# TODO: Now that we have our test cases, let's write some pseudocode. What is your coding approach? How will you
+# make sure that the input string is returned reversed? What logic do you need to include (loop, conditions)?
+# Write your Pseudocode in the commented lines below:
+# 
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+# STEP 3: Code it up!
+# TODO: Great job, we've made it to the actual coding part of this assignment. Using your above test cases and pseudocode,
+# try writing out your solution to the problem. We have included the starter code in Python below for your reference.
+
+def reverse_string(s):
+
+
+
+
+
+
+# STEP 4: Testing Your Code
+# (OPTIONAL) TODO: Convert your above test cases from Step 2 into actual function calls and run your code to check if your
+# function works. We've included an example call below.
+
+print('reverse_string("hi") should return "ih"', reverse_string("hi"))
+
+
+
+
+
+
+
+
+
+  
